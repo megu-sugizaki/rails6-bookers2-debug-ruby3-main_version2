ed9c5f951c3907187a42eb5c474f149085f102bb
EC2 Default User <ec2-user@ip-172-31-9-47.ap-northeast-1.compute.internal>
2023-09-25 12:47:11 +0000

    応用課題　finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only:[:edit, :update]
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
@@ -13,10 +16,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
-      @books = Book.all
-      render 'index'
+      flash.now[:notice]
+      render :index
     end
   end
 
@@ -25,23 +28,32 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book_new = Book.new
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      flash.now[:notice]
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end 
 end
